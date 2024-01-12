2cc88964dd9afc5590093bf65b24bf100d46b030
Robson <robson.lsilva36@gmail.com>
2024-01-12 19:54:22 -0300

    Project created, User entity and resource created

---
+++ src/test/java/com/robson/webservices_spring/WebservicesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robson.webservices_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
