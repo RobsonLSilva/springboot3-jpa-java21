835a74577bc88ba019a584b368a6e549e530905f
Robson <robson.lsilva36@gmail.com>
2024-01-18 17:49:27 -0300

    Exception handling - update

--- src/main/java/com/robson/webservices_spring/services/UserService.java
+++ src/main/java/com/robson/webservices_spring/services/UserService.java
@@ -4,6 +4,7 @@ import com.robson.webservices_spring.entities.User;
 import com.robson.webservices_spring.repositories.UserRepository;
 import com.robson.webservices_spring.services.exceptions.DatabaseException;
 import com.robson.webservices_spring.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -39,9 +40,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj){
